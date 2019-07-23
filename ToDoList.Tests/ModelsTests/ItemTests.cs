b479e64be15efdcb863bba26bae3a417d8c384ea
saswati <p403saswati@gmail.com>
2019-07-23 16:19:28 -0700

    disposable and clear method added

--- ToDoList.Tests/ModelsTests/ItemTests.cs
+++ ToDoList.Tests/ModelsTests/ItemTests.cs
@@ -1,12 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest: IDisposable
   {
-
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
@@ -23,6 +28,14 @@ namespace ToDoList.Tests
       newItem.Description = updatedDescription;
       string result = newItem.Description;
       Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> { };
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,12 +1,25 @@
+using System.Collections.Generic;
+using System;
 namespace ToDoList.Models
 {
   public class Item
   {
     public string Description { get; set;}
+    private static List<Item> _instances = new List<Item> {};
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
 
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this);
+    }
+    public static List<Item> GetAll()
+    {
+      return _instances;
     }
   }
 }
